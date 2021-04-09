cf8c6695f29bd00b21427fd40718480de3df58c1
Jose A. Vallejo <54599840+JoseAVallejo12@users.noreply.github.com>
2021-04-09 09:19:43 -0500

    implement Bulma frameworks CSS done, and responsive desing work done

--- src/app/to-do/list/list.component.ts
+++ src/app/to-do/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import Task from '../models/task.model';
-import { faEdit, faEraser } from '@fortawesome/free-solid-svg-icons'
+import { faEdit, faEraser, faSave } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-list',
@@ -9,13 +9,33 @@ import { faEdit, faEraser } from '@fortawesome/free-solid-svg-icons'
 })
 export class ListComponent implements OnInit {
   @Input() task: Task;
+  iconEdit = faEdit;
+  iconDelete = faEraser;
+  iconSave = faSave;
+  editTask: boolean;
+  classnameOne: string;
+  labelButtonEditSave: string;
 
 
   constructor() {
     this.task = new Task();
-
+    this.editTask = false;
+    this.classnameOne = 'is-outlined'
+    this.labelButtonEditSave = 'Edit'
   }
 
   ngOnInit(): void {}
+
+  onClickEdit(){
+    if (this.editTask) {
+      this.editTask = false;
+      this.classnameOne = 'is-outlined';
+      this.labelButtonEditSave = 'Edit';
+    } else {
+      this.editTask = true;
+      this.classnameOne = ''
+      this.labelButtonEditSave = 'Save';
+    }
+  }
 
 }
